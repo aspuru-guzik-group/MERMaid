22a3fc973b4125a00abad95ff07b6a8f5cb0a081
shixuanleong <shixuan001@e.ntu.edu.sg>
2024-10-01 09:29:37 -0400

    updated TF_to_json.py

--- TF_to_json/TF_to_json.py
+++ TF_to_json/TF_to_json.py
@@ -7,10 +7,9 @@ import cv2
 import numpy as np
 from os import listdir
 import json
-
+import glob
 
 def reformat_json(input_file):
-    
     with open(input_file, 'r') as file:
         json_content = file.read()
         json_content = json_content.replace("\"```json\\n", '').replace('```"', '').strip()       
@@ -22,6 +21,7 @@ def reformat_json(input_file):
     with open(input_file, 'w') as file:
         file.write(formatted_json)
 
+# OLD 
 def image_crop(input_image, image_directory, output_directory):
   image_path = os.path.join(image_directory, f"{input_image}.png")
   image = cv2.imread(image_path)
@@ -66,6 +66,7 @@ def image_crop(input_image, image_directory, output_directory):
   cv2.imwrite(os.path.join(output_directory, f"{input_image}_2.png"), middle_segment)
   cv2.imwrite(os.path.join(output_directory, f"{input_image}_3.png"), bottom_segment)
 
+# OLD
 def batch_image_crop(image_directory, output_directory):
   for image in os.listdir(image_directory): 
     if (image.endswith(".png")):
@@ -81,15 +82,8 @@ def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
   
-
-
-def encode_image(image_path):
-    # Function to encode image in base64
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
-
 def get_data(prompt_directory, image_name, image_directory, json_directory): 
-    api_key = "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"  # Replace with your actual API key
+    api_key = "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" 
 
     # Paths to images
     image_path1 = os.path.join(image_directory, f"{image_name}_1.png")
@@ -202,6 +196,102 @@ def get_data(prompt_directory, image_name, image_directory, json_directory):
     except requests.exceptions.RequestException as e:
         print(f"Error during API request: {e}")
 
+def adaptive_get_data(prompt_directory, image_name, image_directory, json_directory):
+    api_key = "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"
+    
+    # Find all subimages 
+    image_paths = glob.glob(os.path.join(image_directory, f"{image_name}_*.png"))
+    if not image_paths:
+       print(f"No subimages found for {image_name}")
+       return
+    
+    # Encode all images
+    base64_images = []
+    for image_path in image_paths:
+       base64_images.append(encode_image(image_path))
+
+    # Read user message prompt
+    user_prompt_path = os.path.join(prompt_directory, "rxn_opt_prompt_v5-2.txt")
+    with open(user_prompt_path, "r") as file:
+       user_message = file.read().strip()
+
+    # Path for image caption (if any)
+    image_caption_path = os.path.join(image_directory, f"{image_name}.txt")
+
+    # Path for response files 
+    response_path = os.path.join(json_directory, f"{image_name}_response_4o_w_cropping_promptv5-2.json")
+    token_path = os.path.join(f"{json_directory}/token_count/", f"{image_name}_tokencount_4o_w_cropping_promptv5-2.json")
+
+    # Create base message 
+    messages = [{
+       "role": "user",
+       "content": [
+            {
+                "type": "text",
+                "text": user_message
+            }
+        ]
+    }]
+
+    # Add each encoded image as a separate entry
+    for base64_image in base64_images:
+        messages[0]["content"].append({
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}"
+            }
+        })
+
+    # If the image caption file exists, append it to the messages content
+    if os.path.exists(image_caption_path):
+        with open(image_caption_path, "r") as file:
+            image_caption = file.read().strip()
+        messages[0]["content"].append({
+            "type": "text",
+            "text": image_caption
+        })
+        print('Caption appended!')
+    else: 
+        print('No caption found!')
+
+    # API request headers and payload
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+        "model": "gpt-4o-2024-08-06",
+        "messages": messages,
+        "max_tokens": 4000
+    }
+
+    # Send API request
+    try:
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+        response.raise_for_status()  # Raise error if the request failed
+        reaction_data = response.json()['choices'][0]['message']['content']
+        token_count = response.json()['usage']
+
+        # Save responses
+        with open(response_path, 'w') as json_file:
+            json.dump(reaction_data, json_file)
+        print(f"Reaction data saved to {response_path}!")
+
+        with open(token_path, 'w') as token_file: 
+           json.dump(token_count, token_file)
+        print(f"Token count saved to {response_path}!")
+
+        # Clean response
+        try:
+            reformat_json(response_path)
+            print(f"{response_path}: Reaction data cleaned.")
+        except Exception as e:
+            print(f"{response_path}: Reaction data not cleaned. Error: {e}")
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error during API request: {e}")
+
 def update_dict(prompt_directory, json_file, json_directory): 
     api_key = "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"  # Replace with your actual API key
 
@@ -277,30 +367,32 @@ def update_dict(prompt_directory, json_file, json_directory):
         print(f"Error during API request: {e}")
 
 # Batch processing
+def batch_process(prompt_directory, cropped_image_directory, json_directory, ori_image_directory): 
+    # Extract reaction dictionary
+    for file in os.listdir(ori_image_directory):
+        if (file.endswith(".png")):
+            image_name = file.removesuffix('.png')
+            print(f"processing {image_name}")
+            get_data(prompt_directory, image_name, cropped_image_directory, json_directory)
+    print("#####################################")
+
+    # Update reaction dictionary
+    for file in os.listdir(json_directory):
+        if (file.endswith(".json")):
+            json_name = file.removesuffix('.json')
+            print(f"updating {json_name}")
+            update_dict(prompt_directory, json_name, json_directory)
+
 prompt_directory = "/mnt/c/Users/Shi Xuan/OneDrive - University of Toronto/Project_MERLIN/Project_Digital esyn corpus/TF_to_json/"
 cropped_image_directory = "/mnt/c/Users/Shi Xuan/OneDrive - University of Toronto/Project_MERLIN/Project_Digital esyn corpus/TF_to_json/TEST-cropped/"
 json_directory = "/mnt/c/Users/Shi Xuan/OneDrive - University of Toronto/Project_MERLIN/Project_Digital esyn corpus/TF_to_json/TEST-cropped/output_v5/"
 ori_image_directory = "/mnt/c/Users/Shi Xuan/OneDrive - University of Toronto/Project_MERLIN/Project_Digital esyn corpus/TF_to_json/prelim_test/"
 
-# # step 1 
-# for file in os.listdir(ori_image_directory):
-#    if (file.endswith(".png")):
-#       image_name = file.removesuffix('.png')
-#       print(f"processing {image_name}")
-#       get_data(prompt_directory, image_name, cropped_image_directory, json_directory)
-#       print("#############################")
-
-# # step 2 - update dictionary
-# for file in os.listdir(json_directory):
-#    if (file.endswith(".json")):
-#       json_name = file.removesuffix('.json')
-#       print(f"updating {json_name}")
-#       update_dict(prompt_directory, json_name, json_directory)
-
+# Single run
 # response_path = os.path.join(json_directory, "RSC22_table_1_response_4o_w_cropping_promptv4_updated.json")
 # reformat_json(response_path)
 
 #image_name = "wiley23_table_1"
-json_name = "wiley31_actual_table_1_response_4o_w_cropping_promptv5-1"
+# json_name = "wiley31_actual_table_1_response_4o_w_cropping_promptv5-1"
 #get_data(prompt_directory, image_name, cropped_image_directory, json_directory)
-update_dict(prompt_directory, json_name, json_directory)
+# update_dict(prompt_directory, json_name, json_directory)
