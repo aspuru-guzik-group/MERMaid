653eea9ce45cf307158eaa2d3fffb9555e0bd061
Brandon Wong <brandonhywong@gmail.com>
2025-01-16 14:39:31 -0500

    Changed default arguments of wrapper function for PDF-TF-chem

--- PDF-TF-chem/pdf_tf_chem.py
+++ PDF-TF-chem/pdf_tf_chem.py
@@ -4,7 +4,7 @@ import os
 MODEL_ID = "yifeihu/TF-ID-large"
 SAFETENSORS_PATH = "./model_checkpoints/epoch_12/model.safetensors" #currently stored locally on cluster, will upload to huggingface or something?
 
-def pdf_to_figures_and_tables(input_dir: str, output_dir: str, batch: bool):
+def pdf_to_figures_and_tables(input_dir: str, output_dir: str="./", batch: bool=False):
     if batch:
         for file in os.listdir(input_dir): 
             if not file.endswith("pdf"):
