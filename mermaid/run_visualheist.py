608461eba8ddbcf7f456c73d8a0d0c029b5a8bc2
Brandon Wong <brandonhywong@gmail.com>
2025-02-13 15:59:55 -0500

    Added parameter in startup.json to specify model size

--- mermaid/run_visualheist.py
+++ mermaid/run_visualheist.py
@@ -1,5 +1,7 @@
 import json
 from methods_visualheist import batch_pdf_to_figures_and_tables
+import pdb
+import os
 
 def load_config(config_file):
     
@@ -9,11 +11,12 @@ def load_config(config_file):
 
 if __name__ == "__main__":
     # Load the configuration from the file
-    config = load_config('./mermaid/startup.json')
+    package_dir = os.path.dirname(__file__)
+    config = load_config(package_dir + '/../mermaid/startup.json')
 
     # Use the default configuration in the function call if unspecified 
     input_dir = config.get('pdf_dir', "./pdfs")
     output_dir = config.get('image_dir', config.get('default_image_dir'))
-
+    model_size = "large" == config.get('model_size')
     # Use the loaded configuration in the function call
-    batch_pdf_to_figures_and_tables(input_dir, output_dir, large_model=False)
+    batch_pdf_to_figures_and_tables(input_dir, output_dir, large_model=model_size)
