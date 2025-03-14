9c1691793d58f372559c9c44e3d8420c403e7da1
shixuanleong <sleong@DESKTOP-PR3VBOQ.>
2025-03-14 14:40:59 +0100

    changed api location

--- scripts/run_dataraider.py
+++ scripts/run_dataraider.py
@@ -48,7 +48,7 @@ def main():
     parser.add_argument("--json_dir", type=str, help="Directory to save processed JSON data", default=None)
     parser.add_argument("--keys", type=str, nargs='+', help="List of keys to extract", default=None)
     parser.add_argument("--new_keys", type=str, nargs='+', help="List of new keys for data extraction", default=None)
-    parser.add_argument("--api_key", type=str, help="API key", default=None)
+    # parser.add_argument("--api_key", type=str, help="API key", default=None)
     
     args = parser.parse_args()
 
@@ -68,7 +68,8 @@ def main():
         json_dir = config.get('default_json_dir')
     keys = config.get('keys', ["Entry", "Catalyst", "Ligand", "Cathode", "Solvents"])
     new_keys = config.get('new_keys', None)
-    api_key = config.get('api_key', None)
+    # api_key = config.get('api_key', None)
+    api_key = os.environ.get("OPENAI_API_KEY")
 
     info = DataRaiderInfo(api_key=api_key, device="cpu", ckpt_path=ckpt_path)
     # Construct the initial reaction data extraction prompt
@@ -86,30 +87,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-    # # Load the configuration from the file
-    # config = load_config('./mermaid/startup.json')
-
-    # # Use the default configuration in the function call if unspecified 
-    # image_dir = config.get('image_dir', config.get('default_image_dir'))
-    # prompt_dir = config.get('prompt_dir', "./Prompts")
-    # get_data_prompt = "get_data_prompt"
-    # update_dict_prompt = "update_dict_prompt"
-    # output_dir = config.get('json_dir', config.get('default_json_dir'))
-    # keys = config.get('keys', ["Entry", "Catalyst", "Ligand", "Cathode", "Solvents"])
-    # new_keys = config.get('new_keys', None)
-    # api_key = config.get('api_key', None)
-
-    # # Use the loaded configuration in the function call
-    # info = DataRaiderInfo(api_key=api_key, device="cpu", ckpt_path=ckpt_path)
-    # # processor = RxnOptDataProcessor(ckpt_path=ckpt_path, device='cpu', api_key=api_key)
-    # print("Constructing your custom reaction data extraction prompt")
-    # construct_initial_prompt(keys, new_keys)
-    # # processor.construct_initial_prompt(keys, new_keys)
-    # print('######################## Starting up DataRaider ############################')
-    # batch_process_images(info, image_dir, prompt_dir, get_data_prompt, update_dict_prompt, output_dir)
-    # # processor.batch_process_images(image_dir, prompt_dir, get_data_prompt, update_dict_prompt, output_dir)
-    # print('Clearing temporary files and custom prompts')
-    # clear_temp_files(prompt_dir, image_dir)
-    # # processor.clear_temp_files(prompt_dir, image_dir)
