ea570c8bd2cbfe8de68e3315e7fae38a93ff10a7
shixuanleong <sleong@DESKTOP-PR3VBOQ.>
2025-05-18 08:42:25 -0400

    update-dict-with-smiles fix bug

--- src/dataraider/reaction_dictionary_formating.py
+++ src/dataraider/reaction_dictionary_formating.py
@@ -31,7 +31,7 @@ def reformat_json(input_file:str):
         file.write(formatted_json)
 
     
-def update_dict_with_smiles(
+def update_dict_with_smiles_old(
                     info:DataRaiderInfo,
                     image_name:str, 
                     image_directory:str, 
@@ -98,6 +98,74 @@ def update_dict_with_smiles(
         json.dump(updated_dict, output_file, indent = 4)
     print(f'{image_name} reaction dictionary updated with reaction SMILES')
     
+
+def update_dict_with_smiles(
+                    info:DataRaiderInfo,
+                    image_name:str, 
+                    image_directory:str, 
+                    json_directory:str):
+    """
+    Use RxnScribe to get reactants and product SMILES and combine reaction dictionary with reaction SMILES
+    
+    :param info: Global information required for processing
+    :type info: DataRaiderInfo
+    :param image_name: Name of image
+    :type image_name: str
+    :param image_directory: Root directory where the original images are stored
+    :type image_directory: str
+    :param json_directory: Path to directory of reaction dictionary
+    :type json_directory: str
+    
+    :return: Returns nothing, all data saved in JSON
+    :rtype: None
+    """
+    image_file = os.path.join(image_directory, f"/cropped_images/{image_name}_1.png")
+    if not os.path.exists(image_file):
+        image_file = os.path.join(image_directory, f"/cropped_images/{image_name}_original.png")    
+    reactions = []
+
+    # Extract reactant and product SMILES
+    try: 
+        predictions = info.model.predict_image_file(image_file, molscribe=True, ocr=False)
+        for prediction in predictions: 
+            reactant_smiles = [reactant.get('smiles') for reactant in prediction.get('reactants', []) if 'smiles' in reactant]
+            product_smiles = [product.get('smiles') for product in prediction.get('products', []) if 'smiles' in product]
+            reactions.append({'reactants': reactant_smiles, 'products': product_smiles})
+        
+        if not reactions or not reactions[0]['reactants'] or not reactions[0]['products']:
+            reactants, products = 'N.R', 'N.R'
+        else: 
+            reactants = reactions[0]['reactants']
+            products = reactions[0]['products']
+    
+    except Exception as e: 
+        print("No reaction SMILES extracted. Returning 'NR' for reactants and products.")
+        reactants, products = 'N.R', 'N.R'
+
+    # Update reaction dictionary with reaction SMILES 
+    json_path = os.path.join(json_directory, f"{image_name}.json")
+    # json_path = os.path.join(json_directory, f"{image_name}_updated.json") #retrieve updated if saved as new file
+    with open(json_path, 'r') as file: 
+        opt_dict = json.load(file)
+    opt_key = next((k for k in opt_dict if "optimization" in k.lower()), None)
+    if opt_key is None:
+        print(f"WARNING. No optimization key found in the dictionary. Reactant and product SMILES not added")
+        return
+    opt_data = opt_dict[opt_key]
+
+    updated_dict = {
+        "SMILES": {
+            "reactants": reactants, 
+            "products": products
+        }, 
+        "Optimization Runs": opt_data
+    }
+    output_path = json_path # update the original json file
+    # output_path = os.path.join(json_directory, f"{image_name}_updated_smiles.json") #save as new file
+    with open(output_path, 'w') as output_file: 
+        json.dump(updated_dict, output_file, indent = 4)
+    print(f'{image_name} reaction dictionary updated with reaction SMILES')
+
 
 def postprocess_dict(
                      image_name:str,
