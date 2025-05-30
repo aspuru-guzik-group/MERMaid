df80d0996dd7cb5e9dd859f3596598a4c367b104
Brandon Wong <brandonhywong@gmail.com>
2025-05-30 16:13:14 -0400

    Implemented run_kgwizard run file

---
+++ scripts/run_kgwizard.py
@@ -0,0 +1,52 @@
+import json
+import argparse
+from pathlib import Path
+import subprocess
+
+
+def load_config(config_file):
+    """Load configurations from config_file
+
+    :param config_file: Path to config file
+    :type config_file: str
+    :return: Returns a dictionary of fields from config_file
+    :rtype: dict
+    """
+    config_file = Path(config_file)
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+    script_dir = config_file.parent
+    parent_dir = script_dir.parent
+    for key in ['default_image_dir', 'default_json_dir', 'default_graph_dir']:
+        val = config.get(key)
+        if val and not Path(val).is_absolute():
+            config[key] = str((parent_dir / val).resolve())
+    return config
+
+def main():
+    """
+    This function orchestrates loading the configuration, constructing custom prompts of KGWizard
+    """
+    
+    parser = argparse.ArgumentParser(description="Convert information into a graph databse with KGWIzard.")
+    parser.add_argument("--config", type=str, help="Path to the configuration file", default=None)
+    
+    args = parser.parse_args()
+    
+    if args.config:
+        config = load_config(Path(args.config))
+
+    else:
+        package_dir = Path(__file__).resolve().parent.parent
+        config_path = package_dir / "scripts" / "startup.json"
+        config = load_config(config_path) if config_path.exists() else {}
+    
+    
+    json_dir = Path(args.json_dir) if args.json_dir else Path(config.get('json_dir') or config.get('default_json_dir'))
+    output_file = json_dir / "my_graph.graphml"
+
+    subprocess.run(["kgwizard", "transform", "--input_dir", json_dir, "--output_file", output_file])
+    
+
+if __name__ == "__main__":
+    main()
